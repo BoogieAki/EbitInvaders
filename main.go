b43259321e1553c2dce3727c9fe0085ffd3f7852
Aki Pihlajakoski <aki.pihlajakoski@boogiesoftware.com>
2024-11-02 11:28:41 +0200

    add some sounds

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bytes"
+	_ "embed"
+	"fmt"
 	"image/color"
 	"math"
 	"strconv"
 	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/audio"
+	"github.com/hajimehoshi/ebiten/v2/audio/wav"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
@@ -19,6 +24,11 @@ type Enemy struct {
 	x, y float32
 }
 
+type SoundPlayer struct {
+	audioContext *audio.Context
+	players      map[string]*audio.Player
+}
+
 type Game struct {
 	playerX, playerY float32
 	bullets          []*Bullet
@@ -29,13 +39,36 @@ type Game struct {
 	enemiesSpeed     float32
 	gameStatus       int
 	gameLevel        int
+	soundPlayer      *SoundPlayer
 }
+
+//go:embed assets/sound/laserShoot.wav
+var shootSoundByte []byte
+
+//go:embed assets/sound/hitHurt.wav
+var hitSoundByte []byte
+
+//go:embed assets/sound/explosion.wav
+var explosionSoundByte []byte
+
+//go:embed assets/sound/enemyStepDown.wav
+var enemyStepDownSoundByte []byte
+
+//go:embed assets/sound/won.wav
+var wonSoundByte []byte
 
 const (
 	gameTitle = "Ebit Invaders"
 
 	screenWidth  = 640
 	screenHeight = 480
+
+	sampleRate         = 44100
+	soundShoot         = "shoot"
+	soundHit           = "hit"
+	soundExplosion     = "explosion"
+	soundEnemyStepDown = "stepDown"
+	soundWon           = "won"
 
 	gameOngoing   = 0
 	gameWon       = 1
@@ -101,12 +134,14 @@ func (g *Game) handleKeyPress() {
 	if ebiten.IsKeyPressed(ebiten.KeySpace) && time.Since(g.lastFireTime) >= fireCooldown {
 		// fire a new bullet and update the last fire time
 		g.bullets = append(g.bullets, &Bullet{x: g.playerX + playerSize/2 - bulletWidth/2, y: g.playerY})
+		g.soundPlayer.PlaySound(soundShoot)
 		g.lastFireTime = time.Now()
 	}
 	// fire chaingun
 	if ebiten.IsKeyPressed(ebiten.KeyC) && time.Since(g.lastFireTime) >= time.Millisecond*10 {
 		// fire a new bullet and update the last fire time
 		g.bullets = append(g.bullets, &Bullet{x: g.playerX + playerSize/2 - bulletWidth/2, y: g.playerY})
+		g.soundPlayer.PlaySound(soundShoot)
 		g.lastFireTime = time.Now()
 	}
 }
@@ -147,6 +182,7 @@ func (g *Game) handleEnemyMove() {
 		}
 		// move enemies Y
 		if g.enemiesMoveDown {
+			g.soundPlayer.PlaySound(soundEnemyStepDown)
 			enemy.y += enemySize
 		}
 		activeEnemies = append(activeEnemies, enemy)
@@ -187,6 +223,7 @@ func (g *Game) handleCollisions() {
 				bullet.y+bulletHeight > enemy.y {
 				// Collision detected; mark this enemy as hit and don't add it to remainingEnemies
 				hit = true
+				g.soundPlayer.PlaySound(soundHit)
 				break
 			}
 		}
@@ -222,15 +259,18 @@ func (g *Game) handleGameEnd() {
 	// won
 	// TODO: game will never end with WON
 	if len(g.enemies) == 0 && g.gameLevel == gameLevel5 {
+		g.soundPlayer.PlaySound(soundWon)
 		g.gameStatus = gameWon
 	}
 	// next level
 	if len(g.enemies) == 0 {
+		g.soundPlayer.PlaySound(soundWon)
 		g.gameStatus = gameNextLevel
 	}
 	// lost
 	for _, enemy := range g.enemies {
 		if enemy.y >= playerStartY-playerSize {
+			g.soundPlayer.PlaySound(soundExplosion)
 			g.gameStatus = gameLost
 			break
 		}
@@ -293,13 +333,71 @@ func (g *Game) InitializeEnemies(gameLevel int) {
 	}
 }
 
+func NewSoundPlayer(ctx *audio.Context) *SoundPlayer {
+	return &SoundPlayer{
+		audioContext: ctx,
+		players:      make(map[string]*audio.Player),
+	}
+}
+
+// LoadSound loads a .wav sound from byte data and assigns it a name.
+func (sp *SoundPlayer) LoadSound(name string, wavData []byte) error {
+	stream, err := wav.DecodeWithSampleRate(sampleRate, bytes.NewReader(wavData))
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	player, err := sp.audioContext.NewPlayer(stream)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	sp.players[name] = player
+	return nil
+}
+
+// PlaySound plays the sound by name, rewinding it to the beginning each time.
+func (sp *SoundPlayer) PlaySound(name string) {
+	if player, ok := sp.players[name]; ok {
+		player.Rewind()
+		player.Play()
+	}
+}
+
+func loadSounds(soundPlayer *SoundPlayer) error {
+	if err := soundPlayer.LoadSound(soundShoot, shootSoundByte); err != nil {
+		return err
+	}
+	if err := soundPlayer.LoadSound(soundHit, hitSoundByte); err != nil {
+		return err
+	}
+	if err := soundPlayer.LoadSound(soundExplosion, explosionSoundByte); err != nil {
+		return err
+	}
+	if err := soundPlayer.LoadSound(soundEnemyStepDown, enemyStepDownSoundByte); err != nil {
+		return err
+	}
+	if err := soundPlayer.LoadSound(soundWon, wonSoundByte); err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
+	audioContext := audio.NewContext(sampleRate)
+	soundPlayer := NewSoundPlayer(audioContext)
+
+	// Load each sound effect
+	loadSounds(soundPlayer)
+
 	game := &Game{
 		playerX:      playerStartX,
 		playerY:      playerStartY,
 		lastFireTime: time.Now().Add(-fireCooldown),
 		gameLevel:    gameLevel1,
+		soundPlayer:  soundPlayer,
 	}
+
 	game.InitializeEnemies(gameLevel1)
 
 	ebiten.SetWindowSize(screenWidth, screenHeight)
